d58467388a7b106364dce49766eede3a6a3a2da1
SjoerdJakobs <18284@ma-web.nl>
2016-09-23 16:22:54 +0200

    made some variables
    made some variables

---
+++ Assets/City.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class City : MonoBehaviour {
+
+    /// <summary>
+    /// in this script is all the info over the city
+    /// </summary>
+
+    //ints
+    [SerializeField]
+    private int castle = 0;
+    [SerializeField]
+    private int knightsCastle = 0;
+    [SerializeField]
+    private int Wall = 0;
+    [SerializeField]
+    private int secondWall = 0;
+    [SerializeField]
+    private int thirdWall = 0;
+    [SerializeField]
+    private int guardTowers = 0;
+    [SerializeField]
+    private int barracks = 0;
+    [SerializeField]
+    private int patrollCentre = 0;
+    [SerializeField]
+    private int blacksmith = 0;
+    [SerializeField]
+    private int housing = 0;
+    [SerializeField]
+    private int aphothecary = 0;
+    [SerializeField]
+    private int treasurery = 0;
+    [SerializeField]
+    private int granary = 0;
+    [SerializeField]
+    private int storageHouse = 0;
+    [SerializeField]
+    private int tradingGuild = 0;
+    [SerializeField]
+    private int school = 0;
+    [SerializeField]
+    private int mill = 0;
+    [SerializeField]
+    private int goldMine = 0;
+    [SerializeField]
+    private int ironMine = 0;
+    [SerializeField]
+    private int coalMine = 0;
+    [SerializeField]
+    private int woodMill = 0;
+    [SerializeField]
+    private int plains = 0;
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
